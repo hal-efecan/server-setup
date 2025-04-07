6c2729b5a8f064a4f6473475afb7f920dce7fcaf
Halil Efecan <hefecan@gmail.com>
2025-04-07 20:00:08 +0100

    updates

--- bashrc.sh
+++ bashrc.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-sudo apt install -y curl
+sudo apt install -y curl &&
 
 BASHRC_URL="https://raw.githubusercontent.com/hal-efecan/bashrc/main/.bashrc"
 
-curl -o ~/.bashrc "$BASHRC_URL"
+curl -o ~/.bashrc "$BASHRC_URL" &&
 
 source ~/.bashrc
+
+echo "✅ Script complete"
 
 # curl -o ~/.bashrc "https://raw.githubusercontent.com/hal-efecan/bashrc/main/.bashrc"
