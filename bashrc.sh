89aa218d553137091cd7c33c09b213e0d7297bc8
Halil Efecan <hefecan@gmail.com>
2025-04-07 19:55:26 +0100

    cmt  added

--- bashrc.sh
+++ bashrc.sh
@@ -7,3 +7,5 @@ BASHRC_URL="https://raw.githubusercontent.com/hal-efecan/bashrc/main/.bashrc"
 curl -o ~/.bashrc "$BASHRC_URL"
 
 source ~/.bashrc
+
+# curl -o ~/.bashrc "https://raw.githubusercontent.com/hal-efecan/bashrc/main/.bashrc"
