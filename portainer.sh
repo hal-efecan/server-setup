8672c6a939a3f847afa1bc9fffebfe25f8791169
Halil Efecan <hefecan@gmail.com>
2025-04-08 14:37:12 +0100

    added docker + portainer scripts

---
+++ portainer.sh
@@ -0,0 +1,13 @@
+docker volume create portainer_data
+
+docker run -d \ 
+-p 8000:8000 \
+-p 9443:9443 \
+--name portainer \
+--restart=always \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v portainer_data:/data portainer/portainer-ce:lts
+
+# Login to GUI via VM IP
+# https://<your-vm-ip>:9443/
+# Set user credentials
